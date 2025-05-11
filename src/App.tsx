5b86fadd93df8271d0c2d5d93cbf0bb4b49b5c29
Bryan Portillo <bportillo701@gmail.com>
2025-05-11 03:02:56 -0600

    feat: Update favicon link in index.html and add favicon image

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import PersonalTraining from './components/PersonalTraining';
-import Testimonials from './components/Testimonials';
-import HeroSection from './components/HeroSection';
-import styles from './styles/App.module.css';
-import Features from './components/Features';
-import Contact from './components/Contact';
-import Pricing from './components/Pricing';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import PersonalTraining from "./components/PersonalTraining";
+import Testimonials from "./components/Testimonials";
+import HeroSection from "./components/HeroSection";
+import styles from "./styles/App.module.css";
+import Features from "./components/Features";
+import Contact from "./components/Contact";
+import Pricing from "./components/Pricing";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    document.title = "Home";
+  }, []);
+
   return (
     <div className={styles.app}>
       <Navbar />
